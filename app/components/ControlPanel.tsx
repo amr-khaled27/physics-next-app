38c861b2394da3322c8c82e552a0c177bfd534a7
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-31 13:02:53 +0200

    fixed visual bug cause by positioning

--- app/components/ControlPanel.tsx
+++ app/components/ControlPanel.tsx
@@ -42,102 +42,106 @@ const ControlPanel = ({
     <>
       <div
         className={
-          `w-full h-full fixed top-0 left-0 duration-300 ` +
-          (isOpen ? "bg-black/50 backdrop-blur-sm" : "bg-transparent -z-10")
+          `w-screen h-screen fixed top-0 left-0d duration-300 ` +
+          (isOpen ? "bg-black/50 backdrop-blur-sm" : "bg-black/0 -z-10")
         }
       ></div>
       <div
         className={
-          `bg-slate-500 absolute left-0 top-0 min-h-screen w-[300px] duration-300 p-4 text-white ` +
-          (isOpen ? "translate-x-0" : "-translate-x-full")
+          `flex fixed left-0 top-0 gap-4 items-center duration-300 ` +
+          (isOpen ? "translate-x-0" : "-translate-x-[300px]")
         }
       >
-        <button
-          title={isOpen ? "Close panel" : "Open panel"}
-          onClick={clickHandler}
-          className="w-12 h-12 centered text-white rounded-xl hover:bg-slate-700 duration-300 bg-slate-400 -right-14 absolute top-1/2 -translate-y-1/2"
-        >
-          {isOpen ? (
-            <FontAwesomeIcon icon={faAngleLeft} />
-          ) : (
-            <FontAwesomeIcon icon={faAngleRight} />
-          )}
-        </button>
+        <div className="bg-slate-500 h-screen w-[300px] duration-300 z-10 p-4 text-white overflow-y-scroll">
+          <div className="flex flex-col gap-4">
+            <p className="text-center">Create Random Particles</p>
+            <CreateRandom scene={scene} engine={engine} />
 
-        <div className="flex flex-col gap-4">
-          <p className="text-center">Create Random Particles</p>
-          <CreateRandom scene={scene} engine={engine} />
-
-          <div className="flex flex-col">
-            <p className="text-center">Set Gravity</p>
+            <div className="flex flex-col">
+              <p className="text-center">Set Gravity</p>
+            </div>
+            <div className="flex gap-4">
+              <p>X:</p>
+              <input
+                type="range"
+                min="-50"
+                max="50"
+                defaultValue="0"
+                onChange={(e) => {
+                  updateGravityX(parseInt(e.target.value));
+                }}
+                className="range"
+              />
+            </div>
+            <div className="flex gap-4">
+              <p>Y:</p>
+              <input
+                type="range"
+                min="-50"
+                max="50"
+                defaultValue="0"
+                onChange={(e) => {
+                  updateGravityY(parseInt(e.target.value));
+                }}
+                className="range"
+              />
+            </div>
           </div>
-          <div className="flex gap-4">
-            <p>X:</p>
+
+          <label className="label cursor-pointer mt-4">
+            <span className="label-text text-white">Walled</span>
             <input
-              type="range"
-              min="-50"
-              max="50"
-              defaultValue="0"
               onChange={(e) => {
-                updateGravityX(parseInt(e.target.value));
+                toggleWalls(e.target.checked);
               }}
-              className="range"
+              type="checkbox"
+              className="toggle toggle-success"
+              defaultChecked
+            />
+          </label>
+
+          <label className="label cursor-pointer">
+            <span className="label-text text-white">Wrap</span>
+            <input
+              onChange={(e) => {
+                toggleWrap(e.target.checked);
+              }}
+              type="checkbox"
+              className="toggle toggle-success"
+            />
+          </label>
+
+          <div className="flex flex-col mt-4">
+            <p className="text-center">Create Custom Shape</p>
+            <CreateShape
+              createCircle={createCircle}
+              createRectangle={createRectangle}
             />
           </div>
-          <div className="flex gap-4">
-            <p>Y:</p>
-            <input
-              type="range"
-              min="-50"
-              max="50"
-              defaultValue="0"
-              onChange={(e) => {
-                updateGravityY(parseInt(e.target.value));
+
+          <div className="flex justify-end">
+            <button
+              className="bg-red-600 w-12 h-12 rounded-xl centered hover:bg-red-400 transition-colors duration-300 active:scale-95 mt-4"
+              onClick={() => {
+                removeLastParticle();
               }}
-              className="range"
-            />
+            >
+              <FontAwesomeIcon icon={faUndo} />
+            </button>
           </div>
         </div>
 
-        <label className="label cursor-pointer mt-4">
-          <span className="label-text text-white">Walled</span>
-          <input
-            onChange={(e) => {
-              toggleWalls(e.target.checked);
-            }}
-            type="checkbox"
-            className="toggle toggle-success"
-            defaultChecked
-          />
-        </label>
-
-        <label className="label cursor-pointer">
-          <span className="label-text text-white">Wrap</span>
-          <input
-            onChange={(e) => {
-              toggleWrap(e.target.checked);
-            }}
-            type="checkbox"
-            className="toggle toggle-success"
-          />
-        </label>
-
-        <div className="flex flex-col mt-4">
-          <p className="text-center">Create Custom Shape</p>
-          <CreateShape
-            createCircle={createCircle}
-            createRectangle={createRectangle}
-          />
-        </div>
-
-        <div className="flex justify-end">
+        <div className="z-10">
           <button
-            className="bg-red-600 w-12 h-12 rounded-xl centered hover:bg-red-400 transition-colors duration-300 active:scale-95 mt-4"
-            onClick={() => {
-              removeLastParticle();
-            }}
+            title={isOpen ? "Close panel" : "Open panel"}
+            onClick={clickHandler}
+            className="w-12 h-12 centered text-white rounded-xl hover:bg-slate-700 duration-300 bg-slate-400"
           >
-            <FontAwesomeIcon icon={faUndo} />
+            {isOpen ? (
+              <FontAwesomeIcon icon={faAngleLeft} />
+            ) : (
+              <FontAwesomeIcon icon={faAngleRight} />
+            )}
           </button>
         </div>
       </div>
